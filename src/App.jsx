dfcdc201ec1a0d47085f8b5f327879ef39427871
tim.wty <tim.wty@link8tech.tw>
2024-03-08 17:58:20 +0800

    antd / 排版

--- src/App.jsx
+++ src/App.jsx
@@ -1,55 +1,66 @@
 import { useState } from 'react'
 import ballLogo from '/ballLogo.png'
 import './App.css'
-
+import {Input, Pagination} from 'antd';
 import InstagramOutlined from "@ant-design/icons/lib/icons/InstagramOutlined";
 import FacebookOutlined from "@ant-design/icons/lib/icons/FacebookOutlined";
 import YoutubeOutlined from "@ant-design/icons/lib/icons/YoutubeOutlined";
 import MenuFoldOutlined from "@ant-design/icons/lib/icons/MenuFoldOutlined";
 import {videoList} from "./data/data";
+import Search from "antd/es/input/Search.js";
+import {FacebookFilled, InstagramFilled, YoutubeFilled} from "@ant-design/icons";
+import MenuComponent from "./components/Menu.jsx";
 
 function App() {
     const [isHideText, setIsHideText] = useState(true)
 
   return (
     <>
-        <div className="container">
-            <div className="layout header">
-                <div className='title'>
+        <nav className='nav-wrapper'>
+            <div className='title'>
+                {/*<MenuFoldOutlined />*/}
+                <MenuComponent />
+
+                <div className='logo-wrapper'>
                     <a href="https://vitejs.dev">
                         <img src={ballLogo} className="logo" alt="Vite logo" />
                     </a>
-                    <h1>BBall_Vision</h1>
+                    <h2>BBall_Vision</h2>
                 </div>
-                <div className="sub-title">
-                    收錄精華以及您不知道的NBA大小事
-                </div>
-                <h4 className='sub-title'>籃球傳教士 | NBA消息 | 球員報導</h4>
+
             </div>
+        </nav>
+        <div className="header">
+         <Search placeholder="請輸入想要觀看的球星影片關鍵字"  style={{ width: '20rem' }} />
+        </div>
+        <div className="container">
+
             <div className="layout videoContainer">
                 {
                     videoList.map(item=>{
                         return (
                             <div className='video-wrapper'>
-                                <h3>{item.title}</h3>
-                                <iframe
-                                    id="inlineFrameExample"
-                                    title="Inline Frame Example"
-                                    width="99%"
-                                    height="500"
-                                    src={item.videoUrl}l>
-                                </iframe>
+                                {/*<h3>{item.title}</h3>*/}
+                                <div className='iframe-wrapper'>
+                                    <iframe
+                                      id="inlineFrameExample"
+                                      title="Inline Frame Example"
+                                      width="99%"
+                                      height="500"
+                                      src={item.videoUrl}>
+                                    </iframe>
+                                </div>
                                 <div className={isHideText && item.intro.length > 75 ? 'intro-text hide-text':'intro-text'}>
                                     {item.intro}
 
                                 </div>
-                                {
-                                    item.intro.length > 75 && <div className='seeMoreBtn-wrapper'>
-                                        <button
-                                            className='seeMoreBtn'
-                                            onClick={()=>setIsHideText(!isHideText)}>{isHideText ? '查看更多':'查看更少'}</button>
-                                    </div>
-                                }
+                                {/*{*/}
+                                {/*    item.intro.length > 75 && <div className='seeMoreBtn-wrapper'>*/}
+                                {/*        <button*/}
+                                {/*            className='seeMoreBtn'*/}
+                                {/*            onClick={()=>setIsHideText(!isHideText)}>{isHideText ? '查看更多':'查看更少'}</button>*/}
+                                {/*    </div>*/}
+                                {/*}*/}
 
 
                             </div>
@@ -57,23 +68,46 @@ function App() {
                     })
                 }
             </div>
-            <div className="layout SideContainer"></div>
+            {/*<div className="layout SideContainer">*/}
+            {/*    <div>推薦影片</div>*/}
+            {/*    <div>熱門影片</div>*/}
+            {/*</div>*/}
+            <div className='pagination'>
+                <Pagination defaultCurrent={1} total={videoList.length+20}  />
+            </div>
             <div className="layout footer">
-                <div>
-                    <h3 className='about'>關於我們</h3>
-                    <div className="service-wrapper">
-                        <a href="">廣告合作</a>
-                        <a href="">服務條款及隱私權政策</a>
-                        <a href="">聯絡我們</a>
-                        <div>
-                            <InstagramOutlined />
-                            <FacebookOutlined />
-                            <YoutubeOutlined />
+                <div className='about'>
+                    <div>
+                        <div style={{display: 'flex', alignItems: 'center'}}>
+                            <a href="https://vitejs.dev">
+                                <img src={ballLogo} className="logo" alt="Vite logo" />
+                            </a>
+                            <h2 style={{color: '#000'}}>BBall_Vision</h2>
                         </div>
+                    </div>
+
+                    <div className="service-wrapper">
+                        <div>
+                            收錄精華以及您不知道的NBA大小事
+                            <h4 style={{textAlign: 'end'}}>籃球傳教士 | NBA消息 | 球員報導</h4>
+                        </div>
+                        <div style={{display:"flex", flexDirection: 'column', alignItems: 'flex-end'}}>
+                            <a href="">廣告合作</a>
+                            <a href="">服務條款及隱私權政策</a>
+                            <a href="">聯絡我們</a>
+                        </div>
+
                     </div>
                 </div>
                 <div className='copyright'>
-                    @_bball_vision 籃球視界 版權所有
+                    <div>
+                        @_bball_vision 籃球視界 版權所有
+                    </div>
+                    <div>
+                        <FacebookFilled className='social-icon' />
+                        <InstagramFilled className='social-icon' />
+                        <YoutubeFilled className='social-icon' />
+                    </div>
                 </div>
             </div>
         </div>
