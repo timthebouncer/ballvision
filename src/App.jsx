2a4225462c64e1101de05c79fa152cdf97442209
tim.wty <tim.wty@link8tech.tw>
2024-03-13 18:04:18 +0800

    github部屬

--- src/App.jsx
+++ src/App.jsx
@@ -43,9 +43,10 @@ function App() {
                                 {/*<h3>{item.title}</h3>*/}
                                 <div className='iframe-wrapper'>
                                     <iframe
+                                      style={{border:'none'}}
                                       id="inlineFrameExample"
                                       title="Inline Frame Example"
-                                      width="99%"
+                                      width="100%"
                                       height="500"
                                       src={item.videoUrl}>
                                     </iframe>
@@ -77,21 +78,18 @@ function App() {
             </div>
             <div className="layout footer">
                 <div className='about'>
-                    <div>
-                        <div style={{display: 'flex', alignItems: 'center'}}>
-                            <a href="https://vitejs.dev">
-                                <img src={ballLogo} className="logo" alt="Vite logo" />
-                            </a>
-                            <h2 style={{color: '#000'}}>BBall_Vision</h2>
-                        </div>
+                    <div className="footer-logo-wrapper">
+                        <a href="https://vitejs.dev">
+                            <img src={ballLogo} className="logo" alt="Vite logo" />
+                        </a>
+                        <h2 style={{color: '#000'}}>BBall_Vision</h2>
                     </div>
-
                     <div className="service-wrapper">
                         <div>
                             收錄精華以及您不知道的NBA大小事
-                            <h4 style={{textAlign: 'end'}}>籃球傳教士 | NBA消息 | 球員報導</h4>
+                            <h4>籃球傳教士 | NBA消息 | 球員報導</h4>
                         </div>
-                        <div style={{display:"flex", flexDirection: 'column', alignItems: 'flex-end'}}>
+                        <div className="service-info">
                             <a href="">廣告合作</a>
                             <a href="">服務條款及隱私權政策</a>
                             <a href="">聯絡我們</a>
@@ -103,7 +101,7 @@ function App() {
                     <div>
                         @_bball_vision 籃球視界 版權所有
                     </div>
-                    <div>
+                    <div className='social-icon-wrapper'>
                         <FacebookFilled className='social-icon' />
                         <InstagramFilled className='social-icon' />
                         <YoutubeFilled className='social-icon' />
